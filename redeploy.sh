4ebbc80cafaf1de2444db6a5eb3c4f3aef2ff8c4
root <root@vps533568.ovh.net>
2020-05-27 12:36:27 +0200

    added script

--- redeploy.sh
+++ redeploy.sh
@@ -3,4 +3,4 @@
 docker stop homepage
 docker rm homepage
 docker build -t homepage:latest .
-docker run --name homepage --hostname hompage --network web -d --restart unless-stopped homepage:latest
+docker run --name homepage --hostname homepage --network web -d --restart unless-stopped homepage:latest
